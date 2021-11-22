4a07b6042fc6bf61091ff0b8bd2dca80b3ba2d19
Andy <4ndyraul@gmail.com>
2021-11-22 00:29:34 -0800

    basic template ready

---
+++ components/layout.js
@@ -0,0 +1,73 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+const name = "Massick";
+export const siteTitle = "Next.js Sample Website";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className="flex items-center flex-col container max-w-xl px-0 py-4 mx-auto my-auto">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className="flex flex-col items-center">
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className="rounded-full"
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className="text-4xl font-extrabold tracking-tighter my-auto mx-4">
+              {name}
+            </h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className="rounded-full"
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className="text-2xl my-auto mx-4">
+              <Link href="/">
+                <a>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className="ml-12 my-auto mr-0">
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
